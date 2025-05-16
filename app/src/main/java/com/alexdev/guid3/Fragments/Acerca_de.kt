07c86c9a2c1b2874a7e8db46e4f5dbea1e8ba1dc
BOT-Alejandro <hectoralejandro037@gmail.com>
2025-05-16 17:01:43 -0500

    Actualización de version de la app en el Manifest y corrección de obtención de version de app en Acerca_de

--- app/src/main/java/com/alexdev/guid3/Fragments/Acerca_de.kt
+++ app/src/main/java/com/alexdev/guid3/Fragments/Acerca_de.kt
@@ -1,11 +1,10 @@
 package com.alexdev.guid3.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.alexdev.guid3.R
 
 
@@ -14,10 +13,12 @@ class Acerca_de : Fragment(R.layout.fragment_acerca_de) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val txtVersion = view.findViewById<TextView>(R.id.texto_version_app)
+        val versionName = requireContext().packageManager.getPackageInfo(requireContext().packageName, 0).versionName
+        txtVersion.text = versionName
         val toolbar = (activity as? AppCompatActivity)?.findViewById<androidx.appcompat.widget.Toolbar>(R.id.barra_de_herramientas)
         toolbar?.title = "Acerca de"
-
-
-
     }
+
+
 }
